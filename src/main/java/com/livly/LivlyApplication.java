528facefa823b14e9d0b742433b1b0e18cd7b6ed
huyunoki <syu-cream2670@outlook.jp>
2025-06-18 18:53:10 +0900

    最初のコミット

---
+++ src/main/java/com/livly/LivlyApplication.java
@@ -0,0 +1,13 @@
+package com.livly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livly/LivlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
